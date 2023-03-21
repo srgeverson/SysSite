7ecd326998a70de4e0472aa510ea737faf1419d3
geverson.souza <geverson.souza@odontosystem.com.br>
2023-03-20 22:24:55 -0300

    arquivo para testes

---
+++ teste.php
@@ -0,0 +1,12 @@
+<?php 
+    /*
+    * To change this license header, choose License Headers in Project Properties.
+    * To change this template file, choose Tools | Templates
+    * and open the template in the editor.
+    */
+    include_once './br/com/system/assets/php/conf.php';
+    redirect(server_url('?page=ControllerPage&option=home'));
+    //echo 'Ops...' . "\n";
+    //print_r($_ENV);
+    // echo getenv('SENHA')."\n";
+?>
