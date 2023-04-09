6a2ea56ff0843af8de8a41e969f3e42ffc106920
Geverson <geversonjosedesouza@gmail.com>
2023-04-09 11:23:53 -0300

    Remoção de validação antiga

--- view/system/welcome.php
+++ view/system/welcome.php
@@ -5,17 +5,17 @@
  * and open the template in the editor.
  */
 include_once server_path("controller/ControllerFuncionarioUser.php");
-$controllerFuncionarioUser = new ControllerFuncionarioUser();
-global $user_logged;
-if (($controllerFuncionarioUser->searchByFkUser($user_logged->id) == false) && ($user_logged->user_fk_authority_pk_id == 3)) {
-    $controllerSystem = new ControllerSystem();
-    $controllerSystem->funcionario_info('warning=funcionario_not_associated');
-}
+// $controllerFuncionarioUser = new ControllerFuncionarioUser();
+// global $user_logged;
+// if (($controllerFuncionarioUser->searchByFkUser($user_logged->id) == false) && ($user_logged->user_fk_authority_pk_id == 3)) {
+//     $controllerSystem = new ControllerSystem();
+//     $controllerSystem->funcionario_info('warning=funcionario_not_associated');
+// }
 ?>
 <div class="jumbotron">
     <h2>Bem vindo!</h2>
     <hr>
-    <p><?php echo $user_logged->usuario_id;?>
+    <p><?php /*echo $user_logged->usuario_id;*/?>
     </p>
     <a class="btn btn-primary" href="#">Saiba mais...</a>
 </div>
