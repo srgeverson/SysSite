969d30a0ed9349b04a8f5c658ea8b2d8a0a7746c
geverson.souza <geverson.souza@odontosystem.com.br>
2023-04-09 20:03:34 -0300

    movendo arquivos para dentro de seu respectivo projeto

--- sql/log_user.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `log_user` (
-  `luser_id_tabela` int(11) DEFAULT NULL,
-  `luser_fk_usuario_pk_id` int(11) DEFAULT NULL,
-  `luser_operacao` varchar(6) DEFAULT NULL,
-  `luser_campo_modificado` varchar(45) DEFAULT NULL,  
-  `luser_valor_antigo` text,
-  `luser_valor_atual` text,
-  `luser_data_operacao` datetime DEFAULT NULL
-);
